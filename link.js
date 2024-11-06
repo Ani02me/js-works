1face4cff22ac16ce4539ac4eb07e6c19803853a
Ani02me <erbakytelmura@gmail.com>
2024-11-06 12:21:36 +0500

    Add files via upload

---
+++ link.js
@@ -0,0 +1,15 @@
+let a=5, b=10, c=5;
+let isStudent = true;
+let isAlumni = true;
+let san = 0;
+
+
+console.log("san:",!san);
+
+if (a=="") {
+  console.log("Бос жол");
+}
+else{
+  console.log("а-да мен бар");
+
+}
